2098417e492eecb1bee6d2354113fb3e938f99d2
Eduardo Ribas <ribasedu2005@gmail.com>
2023-10-02 21:24:32 -0300

    Otimização do código

--- scripts/app.js
+++ scripts/app.js
@@ -1,63 +1,98 @@
 (() => {
-  const main = document.querySelector('main')
-  const reflectionTrigger = document.querySelectorAll('.reflect')
-  let loading = false
-  
-  const resetPage = () => {
-    const container = document.querySelector('.container')
-    container.classList.add('hidden')
+  const main = document.querySelector("main");
+  let isQuoteLoading = false;
 
+  const createTitle = (text) => {
+    const title = document.createElement("h1");
+    title.textContent = text;
+    title.className = "title";
+    return title;
+  };
 
-    setTimeout(() => {
-      main.removeChild(container)
-      window.location.reload();
-    }, 500)
-  }
+  const createTypography = (text) => {
+    const typography = document.createElement("p");
+    typography.textContent = text;
+    typography.className = "typography";
+    return typography;
+  };
+
+  const createButton = (text, outline) => {
+    const button = document.createElement("button");
+    button.textContent = text;
+    button.className = "button" + (!!outline ? " outline" : "");
+    return button;
+  };
+
+  const createTextWrapper = (titleMessage, bodyMessage) => {
+    const title = createTitle(titleMessage);
+    const body = createTypography(bodyMessage);
+    const textWrapper = document.createElement("div");
+    textWrapper.className = "text-wrapper";
+    textWrapper.append(title, body);
+    return textWrapper;
+  };
+
+  const createContainer = () => {
+    const container = document.createElement("div");
+    return container;
+  };
+
+  const loadNewScreen = ({ oldContainer, newContainer }) => {
+    oldContainer?.classList.add("hidden");
+    clearTimeout(timeoutId);
+
+    var timeoutId = setTimeout(() => {
+      oldContainer && main.removeChild(oldContainer);
+      main.appendChild(newContainer);
+      newContainer.classList.add("container");
+      isQuoteLoading = false;
+    }, 500);
+  };
 
   const handleGenerateQuote = async () => {
-    if (loading) return
-    loading = true
-    const oldContainer = document.querySelector('.container')
-    const response = await fetch('https://api.quotable.io/random').then(e => e.json())
+    if (isQuoteLoading) return;
+    isQuoteLoading = true;
 
-    const newContainer = document.createElement('div')
-    const title = document.createElement('h1')
-    const author = document.createElement('p')
-    const buttonReflectMore = document.createElement('button')
-    const buttonBack = document.createElement('button')
-    const buttonWrapper = document.createElement('div')
-    const textWrapper = document.createElement('div')
-    
-    oldContainer.classList.add('hidden')
+    const oldContainer = document.querySelector(".container");
+    const response = await fetch("https://api.quotable.io/random").then((e) =>
+      e.json()
+    );
 
-    textWrapper.className = 'text-wrapper'
-    title.innerText = response.content
-    title.classList.add('title')
-    author.innerText = `- ${response.author}`
-    author.classList.add('typography')
-    buttonReflectMore.textContent = "Refletir mais"
-    buttonBack.textContent = "Voltar"
+    const generateMoreBtn = createButton("Refletir mais", false);
+    const loadHomeBtn = createButton("Voltar", true);
 
-    buttonReflectMore.onclick = () => handleGenerateQuote()
-    buttonBack.onclick = () => resetPage()
-    
-    textWrapper.append(title, author)
-    buttonBack.className = 'button outline back'
-    buttonReflectMore.className = ('button reflect')
-    buttonWrapper.classList.add('button-wrapper')
-    buttonWrapper.append(buttonReflectMore, buttonBack)
+    generateMoreBtn.onclick = () => handleGenerateQuote();
+    loadHomeBtn.onclick = () => loadNewScreen(generateHome());
 
-    newContainer.classList.add('container')
-    newContainer.append(textWrapper, buttonWrapper)
-    
-    setTimeout(() => {
-      main.removeChild(oldContainer)
-      main.appendChild(newContainer)
-      loading = false
-    }, 500)
-  }
+    const heading = createTextWrapper(response.content, `- ${response.author}`);
 
-  reflectionTrigger.forEach(e => e.addEventListener('click', () => {
-    if (!loading) handleGenerateQuote()
-  }))
-})()
+    const buttonWrapper = document.createElement("div");
+    buttonWrapper.className = "button-wrapper";
+    buttonWrapper.append(generateMoreBtn, loadHomeBtn);
+
+    const newContainer = document.createElement("div");
+    newContainer.append(heading, buttonWrapper);
+
+    loadNewScreen({ oldContainer, newContainer });
+  };
+
+  const generateHome = () => {
+    const heading = createTextWrapper(
+      "O poder da reflexão",
+      "Sabia que tirar um tempinho para relaxar pode te tornar alguém bem mais produtivo? Aproveite esse tempinho para ver algumas frases relaxantes!"
+    );
+    const button = createButton("Refletir um pouco");
+    button.onclick = () => !isQuoteLoading && handleGenerateQuote();
+
+    const newContainer = createContainer();
+    const oldContainer = document.querySelector(".container");
+
+    newContainer.append(heading, button);
+
+    return { oldContainer, newContainer };
+  };
+
+  window.onload = () => {
+    loadNewScreen(generateHome());
+  };
+})();
